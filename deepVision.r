2a5a8aba859f5e5ac5c5cbc1eee5f62e7afc7562
Nils Olav Handegard <nils.olav.handegard@imr.no>
2021-02-25 14:38:12 +0100

    Second version

--- deepVision.r
+++ deepVision.r
@@ -1,8 +1,9 @@
 library(ggplot2)
+require(plyr)
 library(dplyr)
 library(tidyr)
-require(plyr)
 #install.packages("devtools")
+install.packages("ggpubr")
 #devtools::install_github("cardiomoon/ggiraphExtra")
 devtools::install_github("cardiomoon/moonBook")
 require(ggiraph)
@@ -13,6 +14,7 @@ require(ggplot2)
 require(foreign)
 require(MASS)
 
+require(gridExtra)
 setwd('D:/repos/Github/deepVision')
 
 
@@ -38,96 +40,69 @@ ggplot(dat,aes(y=Herring_catch,x=Herring_pred)) +geom_point()+geom_smooth(method
 # Resduals are clearly not ok
 
 # Try to add 1 to a log log rtegression instead
-ggplot(dat,aes(y=log(Herring_catch+1),x=log(Herring_pred+1))) +geom_point()+geom_smooth(method = "glm", formula=y~x-1, se = F)
+ggplot(dat,aes(y=log(Herring_catch+1),x=log(Herring_pred+1))) +geom_point()+geom_smooth(method = "glm", formula=y~x, se = F)
+# this looks somewhat better, but still there is a residual issue
 
-
+#
+# log-log model for stats
+#
 
 # Add in the other covariates
-fit=lm(log(Herring_catch+1)~log(Herring_pred+1)+log(BW_pred+1)+log(Mackerel_pred+1)+0,data=dat)
-summary(fit)
+fit_Herring=lm(log(Herring_catch+1)~log(Herring_pred+1)+log(BW_pred+1)+log(Mackerel_pred+1),data=dat)
+summary(fit_Herring)
+p1<-ggplot(dat,aes(y=log(Herring_catch+1),x=log(Herring_pred+1),col=log(BW_pred+1))) +geom_point()+geom_smooth(method = "glm",  formula=y~x, se = F)
+
+# Blue whiting
+fit_BW=lm(log(BW_catch+1)~log(Herring_pred+1)+log(BW_pred+1)+log(Mackerel_pred+1),data=dat)
+summary(fit_BW)
+p2<-ggplot(dat,aes(y=log(BW_catch+1),x=log(BW_pred+1),col=log(Herring_pred+1))) +geom_point()+geom_smooth(method = "glm",  formula=y~x, se = F)
+
+# Mackerel
+fit_Mackerel=lm(log(Mackerel_catch+1)~log(Herring_pred+1)+log(BW_pred+1)+log(Mackerel_pred+1),data=dat)
+summary(fit_Mackerel)
+p3<-ggplot(dat,aes(y=log(Mackerel_catch+1),x=log(Mackerel_pred+1),col=log(Herring_pred+1))) +geom_point()+geom_smooth(method = "lm",  formula=y~x, se = F)
+
+require(grid)
+require(ggpubr)
+p1
+p2
+p3
 
 
+Predictions, splitting on grouping vars
+library(plyr)
 
-fit3=lm(log(Herring_catch+1)~log(Herring_pred+1)+log(BW_pred+1)+log(Mackerel_pred+1)+0,data=dat)
-summary(fit3)
-dat
+# Using lm, predict mpg from wt, and split on cyl
+# Using dlply will create a list of lm objects for each subset
+models_lm <- dlply(mtcars, "cyl", lm, formula = mpg ~ wt)
 
-ggplot(dat,aes(y=log(Herring_catch+1),x=log(Herring_pred+1),col=log(BW_pred+1))) +geom_point()+geom_smooth(method = "glm",  formula=y~x-1, se = F)
+# Now we use ldply to create a prediction data frame that uses each model
+predicted_lm_cyl <- ldply(models_lm, predictvals, xvar = "wt", yvar = "mpg")
 
+# Finally, make a plot
+p + geom_line(data=predicted_lm_cyl, colour="blue") + facet_wrap(~cyl)
 
+#
+# Linear models for comparison with confusion matrix
+#
 
-ggPredict(fit,interactive=TRUE,data=dat)
+# Linear model
+fit_Herring_lin =lm(Herring_catch ~Herring_pred+BW_pred+Mackerel_pred+0,data=dat)
+summary(fit_Herring_lin)
+fit_BW_lin      =lm(BW_catch      ~Herring_pred+BW_pred+Mackerel_pred+0,data=dat)
+summary(fit_BW_lin)
+fit_Mackerel_lin=lm(Mackerel_catch~Herring_pred+BW_pred+Mackerel_pred+0,data=dat)
+summary(fit_BW_Mackerel)
 
+# Extract linear coefficients
+lin <- fit_Herring_lin$coefficients
+lin <- rbind(lin,fit_BW_lin$coefficients)
+lin <- rbind(lin,fit_Mackerel_lin$coefficients)
 
+# Herring_pred  BW_pred       Mackerel_pred
+# 0.0606497897 -0.005805911   0.008048434
+#-0.0013641536  0.110024823  -0.004669844
+#-0.0008735669  0.002313991   0.024529442
+1/lin
 
-#df <- data.frame(matrix(as.numeric(as.character((unlist(dat)))), nrow=20, byrow=T),stringsAsFactors=FALSE)
-
-#df <- data.frame(matrix(unlist(dat), nrow=20, byrow=T),stringsAsFactors=FALSE)
-#names(df) <- names(dat)
-#df <- sapply(dat, function(x) as.numeric(x))
-#df
-
-
-#data2 <- gather(data,set,count,Both,Årsmaterialet,NMDbiotic)
-#df2 <- gather(df,set,count,Both,Årsmaterialet,NMDbiotic)
-
-
-# Split into pred and observations
-d = NULL
-# The prediction from the deep model
-d$pred <- df[,c(2,4,6)]
-# The count from the trawl
-d$obs <- df[,c(3,5,7)]
-
-#d
-d$bw_obs <- d$obs[,1]
-d$herring_obs <- d$obs[,2]
-d$mackerel_obs <- d$obs[,3]
-
-
-# Overall fraction across stations
-fraction <- colSums(d$pred)/colSums(d$obs)
-print(fraction)
-
-# Run linear model
-lmod <- lm(obs~pred+0,data=d)
-lmodbw <- lm(bw_obs~pred+0,data=d)
-lmodherr <- lm(herring_obs~pred+0,data=d)
-lmodmack <- lm(mackerel_obs~pred+0,data=d)
-summary(lmodbw)
-# Predict
-d$mod <- predict(lmod)
-
-ggPredict(lmodbw,interactive = TRUE)
-
-i=1
-plot(log(d$pred[,i]),log(d$mod[,i]))
-points(log(d$pred[,i]),log(d$obs[,i]),col='red')
-i=2
-plot(d$pred[,i],d$mod[,i])
-points(d$pred[,i],d$obs[,i],col='red')
-i=3
-plot(d$pred[,i],d$mod[,i])
-points(d$pred[,i],d$obs[,i],col='red')
-
-
-#lmodmack$fitted.values
-?ggplot
-ggplot(data=d, aes(bw_obs,pred)) +
-  geom_line()
-
-
-lmod$coefficients
-summary(lmod)
-summary(lmodbw)
-summary(lmodherr)
-summary(lmodmack)
-summary(lmod)
-plot(lmodbw)
-
-summary(lmodherr)
-summary(lmodmack)
-
-plot(lmod)
-?lm
 
